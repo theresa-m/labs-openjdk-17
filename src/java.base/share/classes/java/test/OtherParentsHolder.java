81157a80f7d3d2f40b9cbfc5069aaac0fce2f976
Theresa Mammarella <tmammare@redhat.com>
2022-02-03 16:53:55 -0500

    final try

---
+++ src/java.base/share/classes/java/test/OtherParentsHolder.java
@@ -0,0 +1,9 @@
+package java.test;
+
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.io.Closeable;
+
+public class OtherParentsHolder {
+    //public List<Closeable> otherParents = new ArrayList<>();
+}
